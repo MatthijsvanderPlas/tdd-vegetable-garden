0dd3efd1e458fa79f31c02d10b4d456ae2ae1f94
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-09-04 22:02:36 +0200

    getTotalYield test passed (test 3 and 4)

--- farm.js
+++ farm.js
@@ -1,16 +1,27 @@
+// purpose: get yield for a crop
 // input: a object with a name (crop, plant) and a yield 
 // output: yield
 const getYieldForPlant = (plant) => {
     return plant.yield
 }
 
+// purpose: get yield for a crop object
 // input: object with a crop object {crop, yield} and a numCrops variable 
 // output: yield * numCrops
-const getYieldForCrop = (plant) => {
-    return plant.crop.yield * plant.numCrops
+const getYieldForCrop = (crop) => {
+    return crop.crop.yield * crop.numCrops
 }
+
+// purpose: loop over the array of objects and return the totalyield of the crops passed as objects
+// input: Array of crop objects 
+// output: Total yield as a single number
+const getTotalYield = ({ crops }) => {
+    return  crops.map(crop => getYieldForCrop(crop)).reduce((total, increment) => total + increment)
+}
+
 
 module.exports = {
     getYieldForPlant,
     getYieldForCrop,
+    getTotalYield,
 }
