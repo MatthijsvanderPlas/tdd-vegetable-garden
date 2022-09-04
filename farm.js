118a9872de80da5914e90998ba8cf32a98c5b5e2
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-09-04 21:46:04 +0200

    getYieldForPlant test passed

---
+++ farm.js
@@ -0,0 +1,9 @@
+// input: a object with a name (crop, plant) and a yield 
+// output: yield
+const getYieldForPlant = (plant) => {
+    return plant.yield
+}
+
+module.exports = {
+    getYieldForPlant,
+}
